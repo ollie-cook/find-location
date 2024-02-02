c930a79a8714c246a7203e18756750512d51d8b5
Oliver Cook <olliewatford@gmail.com>
2024-02-02 13:38:35 +0000

    Added 'Built by Ollie'

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <p className="absolute bottom-2 left-1/2 -translate-x-1/2 text-sm" >Built by <a href="https://www.olliecookie.com" className="underline" target="_blank">Ollie Cook</a>&#x1f36a;</p>
         <Analytics />
       </body>
     </html>
